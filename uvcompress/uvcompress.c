30c31c9faeb822ac8605c1687670da4cfe826201
plmurdoch <pmurdoch@uvic.ca>
2023-05-22 22:53:44 -0700

    rough draft of working code for early marking

--- uvcompress/uvcompress.c
+++ uvcompress/uvcompress.c
@@ -1,180 +1,151 @@
 /* uvcompress.c
    CSC 485B/CSC 578B
-   
-   Placeholder starter code for UVCompress 
+
+   Placeholder starter code for UVCompress
    Provided by: B. Bird - 2023-05-01
-   
+
    Student Name: Payton Murdoch
    V#:V00904677
    Due Date: 2023-05-23
 */
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <string.h>
 
-#define INITIAL_ENTRY = 256
-#define FIRST_ENTRY = 257
-#define BASE = 2
-#define INITIAL_BIT = 9
-#define MAX_BIT = 16
-#define INPUT_SIZE = 10
+#define INITIAL_ENTRY 256
+#define FIRST_ENTRY 257
+#define BASE 2
+#define INITIAL_BIT 9
+#define MAX_BIT 16
+#define INPUT_SIZE 10
+#define PACKETIZE 8
 
-typedef struct{
-	char* entry;
-	int index;
-}ITEntry;
-
-void InitIndexTable(ITEntry* I_T,int size);
-void DeallocIndexTable(ITEntry* I_T, int size);
-void GrowTable(ITEntry* I_T,int bits);
-void NewEntry(ITEntry* I_T, char* new_entry, int indx, int size);
-int Comparison(ITEntry* I_T, char* cmpstr,int size);
-void Encode(int next_symbol, int bits, char* str, ITEntry* I_T);
-void ResetStr(char* str);
-void Output(int symbol_number, int curr_bits, int* queue);
-
-
+void Encode(char* str, char** dict, char* working, int length, int next, int bit, int size, int* carryover);
+int Comparison(char** dict, char *str, int size);
+void Output(int number, int bit, int* carryover);
+void ReverseOrder(int* carryover);
 /*
 If combination of char and char +1 in the dictionary output index number
 else add combination to the dictionary with the next available index number
 */
-
 //what to do, create index structure so we know how to add elements to it
 int main(){
-	int init_size = INITIAL_ENTRY;
-	int curr_bits = INITIAL_BIT;
-	int add_index = FIRST_ENTRY;
-	ITEntry* Index_Table = NULL;
-	InitIndexTable(Index_Table, init_size);
-	char next_character = '';
-	char input_string[INPUT_SIZE];
-	
-    while((next_character = fgetc(stdin)) != EOF){
-		strncat(input_string,next_character, 1);
-		if (strlen(input_string) == INPUT_SIZE){
-			Encode(add_index, curr_bits, input_string, Index_Table, init_size);
-			ResetStr(input_string);
-		}
-	}
-	if (strlen(input_string) != 0){
-		Encode(add_index, curr_bits, input_string, Index_Table);
-	}
-}
-//Step 3: construct LZW algorithm (Utilize Sudocode)
-//Step 4: piping contents out to command line (Reverse char bit order, package all 8 bits, reverse 8 bits, output) Note: IF EOF found last bits packed with 0s till 8 bits)
-void Encode(int next_symbol, int bits, char* str, ITEntry* I_T, int size){
-	char* working = "";
-	int saved_bits[8];
-	for(int i = 0; i< strlen(str); i++){
-		char* augmented = strcpy(working);
-		strncat(augmented, &str[i], 1);
-		strncat(augmented, '\0', 1);
-		int index_num = Comparison(I_T, augmented, size);
-		if(index_num != 65536){
-			ResetStr(working);
-			strncpy(working, augmented, strlen(augmented)-1);
-		}
-		else if(next_symbol >= pow(2,16)){
-			int max_case = Comparison(I_T, working, size);
-			output(max_case, saved_bits);
-			ResetStr(working);
-			strncpy(working, str[i], 1);
-		}
-		else{
-			int symbol_id = next_symbol;
-			NewEntry(I_T, augmented, next_symbol,size);
-			output(symbol_id, saved_bits);
-			ResetStr(working)
-			strncpy(working, str[i], 1);
-			if(next_symbol > pow(2,bits)){
-				bits++
-			}
-		}
-	}
-	if (strlen(working) > 0){
-		int last_index = Comparison(I_T, working, size);
-		output(last_index);
-	}		
+    char **dict;
+    dict = malloc(pow(BASE,INITIAL_BIT)*sizeof(char*));
+    for(int i = 0; i <INITIAL_ENTRY; i++){
+		dict[i] = malloc(2*sizeof(char));
+        dict[i][0] = i;
+        dict[i][1] ='\0';
+    }
+    fputc(0x1f,stdout);
+    fputc(0x9d,stdout);
+    fputc(0x90,stdout);
+    int next_symbol = FIRST_ENTRY;
+    int bits = INITIAL_BIT;
+    char next_character;
+    int count = 0;
+    int size = INITIAL_ENTRY;
+    char input_string[10];
+    int length = 2;
+    char *work = malloc(length*sizeof(char));
+    int carry[PACKETIZE];
+    while((next_character = fgetc(stdin))!=EOF){
+		input_string[count] = next_character;
+        count++;
+        if(count == 10){
+			Encode(input_string, dict, work,length, next_symbol, bits, size, carry);
+            count = 0;
+            strncpy(input_string,"",sizeof(input_string));
+        }
+    }
+    if(strlen(work) > 0){
+		int last_num = Comparison(dict,work, size);
+        Output(last_num,bits,carry);
+    }
 }
 
-
-void InitIndexTable(ITEntry* I_T, int size){
-	I_T = (ITEntry*)malloc(INITIAL_MAX *sizeof(ITEntry));
-	for(int i = 0; i <size; i++){
-		I_T[i].entry = (char*)malloc(2*sizeof(char));
-		I_T[i].entry[0] = i;
-		I_T[i].entry[1] = '\0';
-		I_T[i].index = i;
-	}
-	I_T[size].index = size;
-	I_T[size].entry =(char*)malloc(sizeof(char));
-	I_T[size].entry = '\0';
-}
-
-
-void DeallocIndexTable(ITEntry* I_T, int size){
-	for(int i = 0; i < size; i++){
-		free(I_T[i].entry);
-	}
-	free(I_T);
+void Encode(char* str, char** dict, char *working, int length, int next, int bit, int size, int* carryover){
+    for(int i=0; i < INPUT_SIZE; i++){
+		char augmented[length+1];
+        strncpy(augmented, working, length);
+        char str1[2];
+        str1[0] = str[1];
+        str1[1] ='\0';
+        strncat(augmented,str1,2);
+        int index_num = Comparison(dict, augmented, size);
+        if(index_num != 65536){
+			length++;
+            free(working);
+            working = malloc(length*sizeof(char));                       
+			strncpy(working,augmented,length);
+        }else if(next >=pow(BASE,MAX_BIT)){
+			int work_index = Comparison(dict, working, size);
+            Output(work_index,bit, carryover);
+            free(working);
+            working = malloc(length*sizeof(char));
+                        strncpy(working,str1,length);
+        }else{
+			dict[next] = malloc((length+1)*sizeof(char));
+            strncpy(dict[next], augmented, length+1);
+            int comp_index = next;
+            next++;
+            Output(comp_index, bit, carryover);
+            length++;
+            free(working);
+            working = malloc(length*sizeof(char));
+            strncpy(working, augmented, length);
+            if(next > pow(BASE,bit)){
+				bit++;
+                dict = realloc(dict,pow(BASE,bit)*sizeof(char*));
+            }
+        }
+    }
 }
 
 
-void GrowTable(ITEntry* I_T, int bits){
-	I_T = (ITEntry*)realloc(I_T, pow(BASE,bits)*sizeof(ITEntry));
-}
-
-
-void NewEntry(ITEntry* I_T, char* new_entry, int indx, int size){
-	I_T[indx].entry = (char*)malloc(strlen(new_entry)*sizeof(char));
-	I_T[indx].entry = new_entry;
-	I_T[indx].indx = indx;
-	indx++;
-	size++;
-}
-
-
-int Comparison(ITEntry* I_T, char* cmpstr, int size){
-	for (int i = 0; i<size; i++){
-		if(i == 256){
-			//Pass entry
-		}
-		else if (strcmp(I_T[i].entry, cmpstr) ==0){
+int Comparison(char** dict, char *str, int size){
+    for(int i = 0; i<size; i++){
+        if(i == 256){
+			//pass
+        }
+        else if(strcmp(dict[i], str)==0){
 			return i;
-		}
-	}
-	return 65536;
+        }
+    }
+    return 65536;
 }
 
 
-void ResetStr(char *str){
-	char none_string[INPUT_SIZE] = "";
-	strncpy(str, none_string,INPUT_SIZE)	
+
+void Output(int number, int bit,int *carryover){
+    int reverse[bit];
+    int to_bin = number;
+    int count =0;
+    while(to_bin != 0){
+		reverse[count] = to_bin &1;
+        to_bin >>= 1;
+        count++;
+    }
+    int output = sizeof(carryover);
+    for(int i = 0; i< count; i++){
+		if(output != 8){
+			carryover[output] = reverse[i];
+            output++;
+        }else{
+			ReverseOrder(carryover);
+            int blank[8];
+            carryover = blank;
+            carryover[0] = reverse[i];
+            output = 1;
+        }
+    }
 }
 
-void Output(int symbol_number, int curr_bits, int* queue){
-	int become_bin = symbol_number;
-	int reverse_bin[curr_bits]
-	int count = 0;
-	while(become_bin != 0){
-		int bin_digit = reverse_bin % 2;
-		reverse_bin[count] = bin_digit;
-		count++;
-		become_bin =(become_bin-bin_digit)/2
-	}
-	count = sizeof(queue);
-	for(int i = 0; i<sizeof(reverse_bin); i++){
-		queue[count] = reverse_bin[i];
-		count++;
-		if(count == 8){
-			Offload(queue);
-			count = 0;
-		}
-	}
-}
-void Offload(int* queue){
-	for(int i = 7; i>= 0; i--){
-		fwrite(queue[i],sizeof(int),sizeof(queue[i]),stdout);
-	}
-	queue = []
+void ReverseOrder(int *carryover){
+    int count = 0;
+    for(int i = 0; i<8; i++){
+		count += carryover[i]*pow(2,i);
+    }
+    fputc(count,stdout);
 }
