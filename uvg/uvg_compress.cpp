aec2f0f4ff468c30794aa0281aeea12e68bdba88
plmurdoch <pmurdoch@uvic.ca>
2023-07-18 14:09:28 -0700

    Update uvg_compress.cpp

--- uvg/uvg_compress.cpp
+++ uvg/uvg_compress.cpp
@@ -46,7 +46,7 @@ std::vector<std::vector<double>> Coeff(){
     auto results = create_2d_vector<double>(8,8);
     for(int i = 0; i < 8; i++){
         for(int j = 0; j < 8; j++){
-            if(i = 0){
+            if(i == 0){
                 results.at(i).at(j) = sqrt((1/8));
             }else{
                 results.at(i).at(j) = (sqrt((1/4))*cos((((2*j)+1)*i*M_PI)/16));
@@ -82,15 +82,15 @@ std::vector<std::vector<unsigned char>> Quantized(std::vector<std::vector<double
     auto results = create_2d_vector<unsigned char>(8,8);
     for(int i = 0; i<8; i++){
         for(int j = 0; j <8; j++){
-            results.at(i).at(j) = round_and_clamp_to_char((data.at(i).at(j)/Q_1.at(i).at(j)))
+            results.at(i).at(j) = round((data.at(i).at(j)/Q_1.at(i).at(j)));
         }
     }
     return results;
 }
 
 //Function for navigating through the matrices and outputs the results as the encoding order.
-std::vector<unsigned char> E_O(std::vector<std::vector<unsigned char>> data){
-    std::vector<unsigned char> results;
+std::vector<int> E_O(std::vector<std::vector<unsigned char>> data){
+    std::vector<int> results;
     int count = 0;
     int reverse = 1;
     while(count <= 14){
@@ -140,7 +140,7 @@ std::vector<unsigned char> E_O(std::vector<std::vector<unsigned char>> data){
     return results;
 }
 
-void blocks(std::vector<std::vector<PixelYCbCr>> data, int height, int width, std::vector<std::vector<double>> c){
+void blocks(std::vector<std::vector<int>> data, int height, int width, std::vector<std::vector<double>> c){
     int recorded_x = 0;
     int recorded_y = 0;
     while(true){
@@ -149,8 +149,10 @@ void blocks(std::vector<std::vector<PixelYCbCr>> data, int height, int width, st
         int prev = -1;
         for(int i = 0; i<8; i++){
             for(int j = 0; j < 8; j++){
-                if(recorded_y+i <=height){
-                    if(recorded_x+j <= width){
+                if(recorded_y+i < height){
+                    if(recorded_x+j < width){
+                        std::cout<<(recorded_y+i)<<":"<<(recorded_x+j)<<std::endl;
+                        std::cout<<data.at(recorded_y+i).at(recorded_x+j)<<std::endl;
                         temporary.at(i).at(j) = data.at(recorded_y+i).at(recorded_x+j);
                         prev = temporary.at(i).at(j);
                         if(prev_row.size() == 8){
@@ -162,9 +164,6 @@ void blocks(std::vector<std::vector<PixelYCbCr>> data, int height, int width, st
                             temporary.at(i).at(j) = prev;
                             prev_row.push_back(prev);
                         }
-                        else{
-                            exit(1); //error no data
-                        }
                     }
                 }else{
                     for(int k = 0; k< 8; k++){
@@ -175,10 +174,9 @@ void blocks(std::vector<std::vector<PixelYCbCr>> data, int height, int width, st
         }
         std::vector<std::vector<double>> dct_block = DCT(temporary, c);
         std::vector<std::vector<unsigned char>> quantum = Quantized(dct_block);
-        std::vector<unsigned char> encoding = E_O(quantum); //Should be organized in encoding order now for RLE and DC
-        //Next step now, encoding order 
-        if(recorded_x+8 > width){
-            if(recorded_y+8 > height){
+        std::vector<int> encoding = E_O(quantum); //Should be organized in encoding order now for RLE and DC
+        if(recorded_x+8 >=width){
+            if(recorded_y+8 >=height){
                 break;
             }else{
                 recorded_x = 0;
@@ -233,7 +231,6 @@ int main(int argc, char** argv){
     //(Notice that height is the outer dimension, so the pixel at coordinates (x,y) 
     // must be accessed as imageRGB.at(y).at(x)).
     std::vector<std::vector<PixelYCbCr>> imageYCbCr = create_2d_vector<PixelYCbCr>(height,width);
-
     for(unsigned int y = 0; y < height; y++){
         for (unsigned int x = 0; x < width; x++){
             auto [r,g,b] = input_image.get_pixel(x,y);
@@ -241,7 +238,7 @@ int main(int argc, char** argv){
             imageYCbCr.at(y).at(x) = rgb_pixel.to_ycbcr();            
         }
     }
-
+    std::vector<std::vector<double>> coeff = Coeff();
     std::ofstream output_file{output_filename,std::ios::binary};
     OutputBitStream output_stream {output_file};
 
@@ -252,12 +249,13 @@ int main(int argc, char** argv){
     output_stream.push_u32(width);
 
     //Write the Y values 
-    auto y_val = create_2d_vector<unsigned char>(height, width);
+    auto y_val = create_2d_vector<int>(height, width);
     for(unsigned int y = 0; y < height; y++)
-        for (unsigned int x = 0; x < width; x++)
-            y_val.at(y).at(x) - imageYCbCr.at(y).at(x).Y;
+        for (unsigned int x = 0; x < width; x++){
+            y_val.at(y).at(x) = imageYCbCr.at(y).at(x).Y;
             output_stream.push_byte(imageYCbCr.at(y).at(x).Y);
-
+        }
+    blocks(y_val, height, width, coeff);
     //Extract the Cb plane into its own array 
     auto Cb = create_2d_vector<unsigned char>(height,width);
     for(unsigned int y = 0; y < height; y++)
