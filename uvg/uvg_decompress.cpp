5e57fcacfa0bd0fb182e174037e2b70fde5098d9
plmurdoch <pmurdoch@uvic.ca>
2023-07-19 23:45:09 -0700

    Update uvg_decompress.cpp

--- uvg/uvg_decompress.cpp
+++ uvg/uvg_decompress.cpp
@@ -24,7 +24,38 @@
 #include "bitmap_image.hpp"
 #include "uvg_common.hpp"
 
+std::vector<std::pair<int, int>> E_O = {
+    {0,0},{0,1},{1,0},{2,0},{1,1},{0,2},{0,3},{1,2},
+    {2,1},{3,0},{4,0},{3,1},{2,2},{1,3},{0,4},{0,5},
+    {1,4},{2,3},{3,2},{4,1},{5,0},{6,0},{5,1},{4,2},
+    {3,3},{2,4},{1,5},{0,6},{0,7},{1,6},{2,5},{3,4},
+    {4,3},{5,2},{6,1},{7,0},{7,1},{6,2},{5,3},{4,4},
+    {3,5},{2,6},{1,7},{2,7},{3,6},{4,5},{5,4},{6,3},
+    {7,2},{7,3},{6,4},{5,5},{4,6},{3,7},{4,7},{5,6},
+    {6,5},{7,4},{7,5},{6,6},{5,7},{6,7},{7,6},{7,7}
+};
 
+std::vector<std::vector<int>> Q_l = {
+    {16,11,10,16,24,40,51,61},
+    {12,12,14,19,26,58,60,55},
+    {14,13,16,24,40,57,69,56},
+    {14,17,22,29,51,87,80,62},
+    {18,22,37,56,68,109,103,77},
+    {24,35,55,64,81,104,113,92},
+    {49,64,78,87,103,121,120,101},
+    {72,92,95,98,112,100,103,99}
+};
+
+std::vector<std::vector<int>> Q_C = {
+    {17,18,24,47,99,99,99,99},
+    {18,21,26,66,99,99,99,99},
+    {24,26,56,99,99,99,99,99},
+    {47,66,99,99,99,99,99,99},
+    {99,99,99,99,99,99,99,99},
+    {99,99,99,99,99,99,99,99},
+    {99,99,99,99,99,99,99,99},
+    {99,99,99,99,99,99,99,99}
+};
 
 int main(int argc, char** argv){
     if (argc < 3){
@@ -37,14 +68,55 @@ int main(int argc, char** argv){
 
     std::ifstream input_file{input_filename,std::ios::binary};
     InputBitStream input_stream {input_file};
-
+    unsigned int quality = input_stream.read_bits(2);
+    if(quality == 0){
+        for(int i = 0; i< 8; i++){
+            for(int j = 0; j<8; j++){
+                Q_l.at(i).at(j) = (2*Q_l.at(i).at(j));
+                Q_C.at(i).at(j) = (2*Q_C.at(i).at(j)); 
+            }
+        }
+    }else if(quality == 2){
+        for(int i = 0; i< 8; i++){
+            for(int j = 0; j<8; j++){
+                Q_l.at(i).at(j) = (round(0.25*Q_l.at(i).at(j)));
+                Q_C.at(i).at(j) = (round(0.25*Q_C.at(i).at(j))); 
+            }
+        }
+    }
     unsigned int height = input_stream.read_u32();
     unsigned int width = input_stream.read_u32();
-    
+    int  Y_count = ((ceil(height*0.125)*ceil(width*0.125))*64);
+    int Cr_Cb = ceil(((width+1)/2)*0.125)*ceil(((height+1)/2)*0.125)*64;
     auto Y = create_2d_vector<unsigned char>(height,width);
     auto Cb_scaled = create_2d_vector<unsigned char>((height+1)/2,(width+1)/2);
     auto Cr_scaled = create_2d_vector<unsigned char>((height+1)/2,(width+1)/2);
-
+    std::vector<unsigned char> encoded;
+    int count = 0;
+    while(count<Y_count){
+        unsigned char num = input_stream.read_byte();
+        encoded.push_back(encoded);
+        std::vector<unsigned char> length;
+        length.push_back(input_stream.read_bit());
+        if(length.at(0) != 0){
+            unsigned char unary = input_stream.read_bit();
+            while(unary != 0){
+                length.push_back(unary);
+                unary = input_stream.read_bit();
+            }
+            int bits = length.size();
+            std::vector<unsigned char> length_bits;
+            for(int i = 0; i<bits-1; i++){
+                length_bits.push_back(input_stream.read_bit());
+            }
+            //compute length of repetitions.
+        }else{
+            if(encoded.size() == 64){
+                //fill next matrix
+            }
+            count++;
+        }
+    }
     for (unsigned int y = 0; y < height; y++)
         for (unsigned int x = 0; x < width; x++)
             Y.at(y).at(x) = input_stream.read_byte();
@@ -67,7 +139,6 @@ int main(int argc, char** argv){
             };
         }
     }
-    
 
     input_stream.flush_to_byte();
     input_file.close();
