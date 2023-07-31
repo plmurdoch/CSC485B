027f59eba1f200b923970184374304615f8bbebe
plmurdoch <pmurdoch@uvic.ca>
2023-07-31 12:29:06 -0700

    Added A3 implementation allowing for I-frame encoding scheme.
    Now need P-frames which will add overhead.

--- uvid/uvid_compress.cpp
+++ uvid/uvid_compress.cpp
@@ -23,9 +23,231 @@
 #include <cassert>
 #include <cstdint>
 #include <tuple>
+#include <cmath>
 #include "output_stream.hpp"
 #include "yuv_stream.hpp"
 
+//Convenience function to wrap around the nasty notation for 2d vectors
+template<typename T>
+std::vector<std::vector<T> > create_2d_vector(unsigned int outer, unsigned int inner){
+    std::vector<std::vector<T> > V {outer, std::vector<T>(inner,T() )};
+    return V;
+}
+
+//Make into matrix so that it only needs to be computed once instead of multiple computations and function calls.
+std::vector<std::vector<double>> Coeff(){
+    auto results = create_2d_vector<double>(8,8);
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            if(i == 0){
+                results.at(i).at(j) = sqrt(0.125);
+            }else{
+                results.at(i).at(j) = (sqrt((0.25))*cos((((2.00*j)+1.00)*i*M_PI)/16.00));
+            }
+        }
+    }
+    return results;
+}
+
+/*
+Note: There were interesting interactions which resulted in me creating 2 DCT functions.
+For the the High setting worked in a different way in comparison to the medium and low settings.
+This was found after many hours of trial and error in the debugging process. Utilizing the method:
+DCT(A) = CAC^T yielded a result in the high matrix which turned the entire image green.
+However, utilizing a method DCT(A) = ACC^T kept the matrices in tact. I cannot explain how this occured.
+Therefore to compensate for this we have DCT_low for medium and low values and DCT_high for the high setting values.  
+*/
+//Compute DCT(A) = matrix(C)*matrix(A)*matrix(C transpose) and store in results; 
+std::vector<std::vector<double>> DCT_low(std::vector<std::vector<int>> data, std::vector<std::vector<double>> c){
+    auto results = create_2d_vector<double>(8,8);
+    auto temp = create_2d_vector<double>(8,8);
+    for(int i = 0; i< 8; i++){
+        for(int j = 0; j<8; j++){
+            for(int x = 0; x<8; x++){
+                temp.at(i).at(j) += (data.at(x).at(j)*c.at(i).at(x));
+            }
+        }
+    }
+    for(int i = 0; i< 8; i++){
+        for(int j = 0; j<8; j++){
+            for(int x = 0; x<8; x++){
+                results.at(i).at(j) += (temp.at(i).at(x)*c.at(j).at(x)); //for c transpose
+            }
+        }
+    }
+    return results;
+}
+
+std::vector<std::vector<double>> DCT_high(std::vector<std::vector<int>> data, std::vector<std::vector<double>> c){
+    auto results = create_2d_vector<double>(8,8);
+    auto temp = create_2d_vector<double>(8,8);
+    for(int i = 0; i< 8; i++){
+        for(int j = 0; j<8; j++){
+            for(int x = 0; x<8; x++){
+                temp.at(i).at(j) += (data.at(i).at(x)*c.at(x).at(j));
+            }
+        }
+    }
+    for(int i = 0; i< 8; i++){
+        for(int j = 0; j<8; j++){
+            for(int x = 0; x<8; x++){
+                results.at(i).at(j) += (temp.at(i).at(x)*c.at(j).at(x)); //for c transpose
+            }
+        }
+    }
+    return results;
+}
+
+//Quantized values are rounded and clamped within the (-127,127) range so that they can be output as bytes in 0-255 range.
+std::vector<std::vector<int>> Quantized(std::vector<std::vector<double>> data, std::vector<std::vector<int>> Q){
+    auto results = create_2d_vector<int>(8,8);
+    for(int i = 0; i<8; i++){
+        for(int j = 0; j <8; j++){
+            int dat = round((data.at(i).at(j)/Q.at(i).at(j)));
+            if(dat < -127){
+                dat = 0;
+            }else if(dat > 127){
+                dat = 255;
+            }else{
+                dat = dat +127;
+            }
+            results.at(i).at(j) = dat;
+        }
+    }
+    return results;
+}
+
+//Function for navigating through the matrices and outputs the results as the encoding order.
+std::vector<int> E_O(std::vector<std::vector<int>> data){
+    std::vector<int> results;
+    int count = 0;
+    int reverse = 1;
+    while(count <= 14){
+        int i = 0;
+        int j = 0;
+        if(count < 8){
+            if((count % 2) == 0){
+                i = count;
+                j = 0;
+                for(int k = 0; k <= count; k++){
+                    results.push_back(data.at(i).at(j));
+                    i-=1;
+                    j+=1;
+                }
+            }else{
+                i = 0;
+                j = count;
+                for(int k = 0; k <= count; k++){
+                    results.push_back(data.at(i).at(j));
+                    i+=1;
+                    j-=1;
+                }
+            }
+        }
+        else{
+            if((count % 2) == 0){
+                i = 7;
+                j = count-7;
+                for(int k = 0; k < (count-reverse); k++){
+                    results.push_back(data.at(i).at(j));
+                    i-=1;
+                    j+=1;
+                }
+            }else{
+                i = count - 7;
+                j = 7;
+                for(int k = 0; k <(count-reverse); k++){
+                    results.push_back(data.at(i).at(j));
+                    i+=1;
+                    j-=1;
+                }
+            }
+            reverse += 2;
+        }
+        count++;
+    }
+    return results;
+}
+
+//RLE encoding using basic implementation outputing num:length pairs with each entry of the pair utilizing 8 bits.
+//Will be improved in Assignment 4, however, my other implementation for Variable length RLE was not working properly.
+void rle(std::vector<int> data, OutputBitStream stream){
+    int size = data.size();
+    for(int i = 0; i<size; i++){
+        unsigned char output = data.at(i);
+        stream.push_byte(output);
+        int j = i+1;
+        int count = 0;
+        while(j < size){
+            if(data.at(j) == data.at(i)){
+                count++;
+                j++;
+            }else{
+                break;
+            }
+        }
+        if(count == 0){
+            stream.push_byte((unsigned char)0);
+        }else{
+            stream.push_byte((unsigned char)count);
+            i += count;
+        }
+    }
+}
+
+//Main body of code, blocks takes the input and parses them into 8x8 blocks for the dct and quantization operations to occur.
+//In this function the block is placed into encoding order and then RLE is done before the block is streamed into the file.
+void blocks(std::vector<std::vector<int>> data, int height, int width, std::vector<std::vector<double>> c, OutputBitStream stream, std::vector<std::vector<int>> Q, std::string quality){
+    int recorded_x = 0;
+    int recorded_y = 0;
+    while(true){
+        auto temporary = create_2d_vector<int>(8,8);
+        std::vector<int> prev_row;
+        int prev = -1;
+        for(int i = 0; i<8; i++){
+            for(int j = 0; j < 8; j++){
+                if(recorded_y+i < height){
+                    if(recorded_x+j < width){
+                        temporary.at(i).at(j) = data.at(recorded_y+i).at(recorded_x+j);
+                        prev = temporary.at(i).at(j);
+                        if(prev_row.size() == 8){
+                            prev_row.clear();
+                        }
+                        prev_row.push_back(temporary.at(i).at(j));
+                    }else{
+                        if(prev != -1){
+                            temporary.at(i).at(j) = prev;
+                            prev_row.push_back(prev);
+                        }
+                    }
+                }else{
+                    for(int k = 0; k< 8; k++){
+                        temporary.at(i).at(k) = prev_row.at(k);
+                    }
+                }
+            }
+        }
+        auto dct_block = create_2d_vector<double>(8,8);
+        if(quality=="high"){
+            dct_block = DCT_high(temporary, c);
+        }else{
+            dct_block = DCT_low(temporary, c);
+        }
+        std::vector<std::vector<int>> quantum = Quantized(dct_block, Q);
+        std::vector<int> encoding = E_O(quantum);
+        rle(encoding, stream);
+        if(recorded_x+8 >=width){
+            if(recorded_y+8 >=height){
+                break;
+            }else{
+                recorded_x = 0;
+                recorded_y += 8;
+            }
+        }else{
+            recorded_x += 8;
+        }
+    }
+}
 
 int main(int argc, char** argv){
 
@@ -40,21 +262,68 @@ int main(int argc, char** argv){
     YUVStreamReader reader {std::cin, width, height};
     OutputBitStream output_stream {std::cout};
 
+    std::vector<std::vector<double>> coeff = Coeff();
+    
+    //After trial and error I decided to utilize a single quantum value.
+    //I utilized the resouce below to better understand standard quantum matrices.
+    //I then create one that worked well with my DCT to limit the number of values which exceed the range of -127 and 127. 
+    //https://cs.stanford.edu/people/eroberts/courses/soco/projects/data-compression/lossy/jpeg/coeff.htm 
+    std::vector<std::vector<int>> Q = {
+        {8,16,19,22,26,27,29,34},
+        {16,16,22,24,27,29,34,37},
+        {19,22,26,27,29,34,34,38},
+        {22,22,26,27,29,34,37,40},
+        {22,26,27,29,32,35,40,48},
+        {26,27,29,32,35,40,48,58},
+        {26,27,29,34,38,46,56,69},
+        {27,29,35,38,46,56,69,83}
+    };
+    //the next for loops allow us to change the quality setting of our Quantum based on the user input.
+    if(quality == "low"){
+        for(int i = 0; i< 8; i++){
+            for(int j = 0; j<8; j++){
+                Q.at(i).at(j) = round(2*(Q.at(i).at(j))); 
+            }
+        }
+        output_stream.push_byte((unsigned char)0);
+        
+    }else if(quality == "high"){
+        for(int i = 0; i< 8; i++){
+            for(int j = 0; j<8; j++){
+                Q.at(i).at(j) = (round(0.2*Q.at(i).at(j)));
+            }
+        }
+        output_stream.push_byte((unsigned char)2);
+    }else{
+        for(int i = 0; i< 8; i++){
+            for(int j = 0; j<8; j++){
+                Q.at(i).at(j) = ((Q.at(i).at(j)+8));     
+            }
+        }
+        output_stream.push_byte((unsigned char)1);
+    }
+
     output_stream.push_u32(height);
     output_stream.push_u32(width);
 
     while (reader.read_next_frame()){
         output_stream.push_byte(1); //Use a one byte flag to indicate whether there is a frame here
         YUVFrame420& frame = reader.frame();
+        auto y_val = create_2d_vector<int>(height, width);
         for (u32 y = 0; y < height; y++)
             for (u32 x = 0; x < width; x++)
-                output_stream.push_byte(frame.Y(x,y));
+                y_val.at(y).at(x) = frame.Y(x,y);
+        blocks(y_val, height, width, coeff, output_stream, Q, quality);
+        auto Cb = create_2d_vector<int>((height/2),(width/2));
         for (u32 y = 0; y < height/2; y++)
             for (u32 x = 0; x < width/2; x++)
-                output_stream.push_byte(frame.Cb(x,y));
+                Cb.at(y).at(x) = frame.Cb(x,y);
+        blocks(Cb, ((height)/2), ((width)/2), coeff, output_stream, Q, quality);
+        auto Cr = create_2d_vector<int>((height/2),(width/2));
         for (u32 y = 0; y < height/2; y++)
             for (u32 x = 0; x < width/2; x++)
-                output_stream.push_byte(frame.Cr(x,y));
+                Cr.at(y).at(x) = frame.Cr(x,y);
+        blocks(Cr, ((height)/2), ((width)/2), coeff, output_stream, Q, quality);
     }
 
     output_stream.push_byte(0); //Flag to indicate end of data
--- uvid/uvid_decompress.cpp
+++ uvid/uvid_decompress.cpp
@@ -22,8 +22,157 @@
 #include <cassert>
 #include <cstdint>
 #include <tuple>
+#include <cmath>
 #include "input_stream.hpp"
 #include "yuv_stream.hpp"
+
+//Convenience function to wrap around the nasty notation for 2d vectors
+template<typename T>
+std::vector<std::vector<T> > create_2d_vector(unsigned int outer, unsigned int inner){
+    std::vector<std::vector<T> > V {outer, std::vector<T>(inner,T() )};
+    return V;
+}
+
+//Global vector which stores the encoding order for 8x8 Quantized DCT matrices.
+std::vector<std::pair<int, int>> E_O = {
+    {0,0},{0,1},{1,0},{2,0},{1,1},{0,2},{0,3},{1,2},
+    {2,1},{3,0},{4,0},{3,1},{2,2},{1,3},{0,4},{0,5},
+    {1,4},{2,3},{3,2},{4,1},{5,0},{6,0},{5,1},{4,2},
+    {3,3},{2,4},{1,5},{0,6},{0,7},{1,6},{2,5},{3,4},
+    {4,3},{5,2},{6,1},{7,0},{7,1},{6,2},{5,3},{4,4},
+    {3,5},{2,6},{1,7},{2,7},{3,6},{4,5},{5,4},{6,3},
+    {7,2},{7,3},{6,4},{5,5},{4,6},{3,7},{4,7},{5,6},
+    {6,5},{7,4},{7,5},{6,6},{5,7},{6,7},{7,6},{7,7}
+};
+
+//Function for building the Coefficient matrix to utilize in quantization
+std::vector<std::vector<double>> Coeff(){
+    auto results = create_2d_vector<double>(8,8);
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            if(i == 0){
+                results.at(i).at(j) = sqrt(0.125);
+            }else{
+                results.at(i).at(j) = (sqrt((0.25))*cos((((2.00*j)+1.00)*i*M_PI)/16.00));
+            }
+        }
+    }
+    return results;
+}
+
+/*
+Note: There were interesting interactions which resulted in me creating 2 DCT functions.
+For the the High setting worked in a different way in comparison to the medium and low settings.
+This was found after many hours of trial and error in the debugging process. Utilizing the method:
+DCT(A) = CAC^T yielded a result in the high matrix which turned the entire image green.
+However, utilizing a method DCT(A) = ACC^T kept the matrices in tact. I cannot explain how this occured.
+Therefore to compensate for this we have inverse_DCT_low for medium and low values and inverse_DCT_high for the high setting values.  
+*/
+std::vector<std::vector<double>> inverse_DCT_low(std::vector<std::vector<int>> data, std::vector<std::vector<double>> c){
+    auto results = create_2d_vector<double>(8,8);
+    auto temp = create_2d_vector<double>(8,8);
+    for(int i = 0; i< 8; i++){
+        for(int j = 0; j<8; j++){
+            for(int x = 0; x<8; x++){
+                temp.at(i).at(j) += (data.at(x).at(j)*c.at(x).at(i));//for c transpose
+            }
+        }
+    }
+    for(int i = 0; i< 8; i++){
+        for(int j = 0; j<8; j++){
+            for(int x = 0; x<8; x++){
+                results.at(i).at(j) += (temp.at(i).at(x)*c.at(x).at(j)); 
+            }
+        }
+    }
+    return results;
+}
+
+std::vector<std::vector<double>> inverse_DCT_high(std::vector<std::vector<int>> data, std::vector<std::vector<double>> c){
+    auto results = create_2d_vector<double>(8,8);
+    auto temp = create_2d_vector<double>(8,8);
+    for(int i = 0; i< 8; i++){
+        for(int j = 0; j<8; j++){
+            for(int x = 0; x<8; x++){
+                temp.at(i).at(j) += (data.at(i).at(x)*c.at(j).at(x));//for c transpose
+            }
+        }
+    }
+    for(int i = 0; i< 8; i++){
+        for(int j = 0; j<8; j++){
+            for(int x = 0; x<8; x++){
+                results.at(i).at(j) += (temp.at(i).at(x)*c.at(x).at(j)); 
+            }
+        }
+    }
+    return results;
+}
+
+//Main body for the code read_input reads the input until the associated matrix of size height and width is filled completely.
+//Number:length pair bytes are read and then stored in the encoded vector, once the encoded vector has enough entries for an 8x8 matrix they are offloaded into a DCT matrix.
+//DCT matrix is then inverted using our coefficient in the order based on the quality. Data is then rounded and bounded to the range 0-255 for image reconstruction.
+std::vector<std::vector<unsigned char>> read_input(InputBitStream input, std::vector<std::vector<int>> Q, std::vector<std::vector<double>> C, int height, int width, int quality){
+    auto result = create_2d_vector<unsigned char>(height, width);
+    int counter = 0;
+    std::vector<unsigned char> encoded;
+    int recorded_y = 0;
+    int recorded_x = 0;
+    while(true){
+        unsigned char num = input.read_byte();
+        encoded.push_back(num);
+        unsigned char length = input.read_byte();
+        int value = length;
+        if(value != 0){
+            for(int j = 0; j<value; j++){
+                encoded.push_back(num);
+            }
+        }
+        int encoded_size = encoded.size();
+        if(encoded_size == 64){
+            auto DCT = create_2d_vector<int>(8,8);
+            int E_O_size = E_O.size();
+            for(int i = 0; i<E_O_size; i++){
+                int row = E_O.at(i).first;
+                int column = E_O.at(i).second;
+                int for_DCT = encoded.at(i);
+                DCT.at(row).at(column) = ((for_DCT-127)*Q.at(row).at(column));
+            }
+            encoded.clear();
+            auto data = create_2d_vector<double>(8,8);
+            if(quality != 2){
+                data = inverse_DCT_low(DCT, C);
+            }else{
+                data = inverse_DCT_high(DCT, C);
+            }
+            for(int y = 0; y < 8; y++){
+                for(int x = 0; x<8; x++){
+                    if((y+recorded_y) < height){
+                        if((x+recorded_x) < width){
+                            int dat = round(data.at(y).at(x));
+                            if(dat >255){
+                                dat = 255;
+                            }else if(dat <0){
+                                dat = 0;
+                            }
+                            result.at(y+recorded_y).at(x+recorded_x) = dat;
+                            if (counter == ((height*width)-1)){
+                                return result;
+                            }else{
+                                counter++;
+                            }
+                        }
+                    }
+                }
+            }
+            if((recorded_x+8) < width){
+                recorded_x += 8;
+            }else{
+                recorded_x = 0;
+                recorded_y += 8;
+            }
+        }
+    }
+}
 
 
 int main(int argc, char** argv){
@@ -32,23 +181,65 @@ int main(int argc, char** argv){
     //      it needs to know about the data must be encoded into the bitstream)
     
     InputBitStream input_stream {std::cin};
-
+    unsigned int quality = input_stream.read_byte();
+    //After trial and error I decided to utilize a single quantum value.
+    //I utilized the resouce below to better understand standard quantum matrices.
+    //I then create one that worked well with my DCT to limit the number of values which exceed the range of -127 and 127. 
+    //https://cs.stanford.edu/people/eroberts/courses/soco/projects/data-compression/lossy/jpeg/coeff.htm 
+    std::vector<std::vector<int>> Q = {
+        {8,16,19,22,26,27,29,34},
+        {16,16,22,24,27,29,34,37},
+        {19,22,26,27,29,34,34,38},
+        {22,22,26,27,29,34,37,40},
+        {22,26,27,29,32,35,40,48},
+        {26,27,29,32,35,40,48,58},
+        {26,27,29,34,38,46,56,69},
+        {27,29,35,38,46,56,69,83}
+    };
+    //the next for loops allow us to change the quality setting of our Quantum based on the user input.
+    if(quality == 0){
+        for(int i = 0; i< 8; i++){
+            for(int j = 0; j<8; j++){
+                Q.at(i).at(j) = (2*Q.at(i).at(j));
+            }
+        }
+    }else if(quality == 2){
+        for(int i = 0; i< 8; i++){
+            for(int j = 0; j<8; j++){
+                Q.at(i).at(j) = (round((0.2*Q.at(i).at(j)))); 
+            }
+        }
+    }else{
+        for(int i = 0; i< 8; i++){
+            for(int j = 0; j<8; j++){
+                Q.at(i).at(j) = ((Q.at(i).at(j)+8));    
+            }
+        }
+    }
+    std::vector<std::vector<double>> coeff = Coeff();
     u32 height {input_stream.read_u32()};
     u32 width {input_stream.read_u32()};
-
     YUVStreamWriter writer {std::cout, width, height};
-
     while (input_stream.read_byte()){
+        auto Y = create_2d_vector<unsigned char>(height,width);
+        auto Cb = create_2d_vector<unsigned char>((height)/2,(width)/2);
+        auto Cr = create_2d_vector<unsigned char>((height)/2,(width)/2);
+        //Build Y matrix.
+        Y = read_input(input_stream, Q, coeff,height, width, quality);
+        //Build Cb matrix.
+        Cb = read_input(input_stream, Q, coeff,((height)/2),((width)/2), quality);
+        //Build Cr matrix.
+        Cr = read_input(input_stream, Q, coeff,((height)/2),((width)/2), quality);
         YUVFrame420& frame = writer.frame();
         for (u32 y = 0; y < height; y++)
             for (u32 x = 0; x < width; x++)
-                frame.Y(x,y) = input_stream.read_byte();
+                frame.Y(x,y) = Y.at(y).at(x);
         for (u32 y = 0; y < height/2; y++)
             for (u32 x = 0; x < width/2; x++)
-                frame.Cb(x,y) = input_stream.read_byte();
+                frame.Cb(x,y) = Cb.at(y).at(x);
         for (u32 y = 0; y < height/2; y++)
             for (u32 x = 0; x < width/2; x++)
-                frame.Cr(x,y) = input_stream.read_byte();
+                frame.Cr(x,y) = Cr.at(y).at(x);
         writer.write_frame();
     }
 
